c70a0493f8c11c3182bad7fe3fdaaff65c978417
miaolz123 <miaolizhao@126.com>
2018-09-07 20:53:04 +0800

    feat: can choose docker install

--- init.linux.sh
+++ init.linux.sh
@@ -1,9 +1,38 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-INITPATH=$(
-  cd $(dirname $0)
-  pwd
-)
+set -u
+
+INIT_PATH=$(cd $(dirname $0) && pwd)
+INSTALL_DOCKER=0
+
+help() {
+  cat << EOF
+usage: $0 [OPTIONS]
+    --help               Show this message
+    --all                Install all tools auto
+    --docker             Install docker auto
+EOF
+}
+
+for opt in "$@"; do
+  case $opt in
+    --help)
+      help
+      exit 0
+      ;;
+    --all)
+      INSTALL_DOCKER=1
+      ;;
+    --docker)
+      INSTALL_DOCKER=1
+      ;;
+    *)
+      echo "unknown option: $opt"
+      help
+      exit 1
+      ;;
+  esac
+done
 
 if [[ -e /etc/debian_version ]]; then
   OS=debian
@@ -55,7 +84,7 @@ if !(which tmux >/dev/null 2>&1); then
 fi
 
 if [ ! -e "$HOME/.tmux.conf" ]; then
-  ln -sf "$INITPATH/tmux.conf" "$HOME/.tmux.conf"
+  ln -sf "$INIT_PATH/tmux.conf" "$HOME/.tmux.conf"
 fi
 
 echo '
@@ -69,7 +98,7 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
 fi
 
 if [ ! -e "$HOME/.localrc" ]; then
-  cp "$INITPATH/localrc.zsh" "$HOME/.localrc"
+  cp "$INIT_PATH/localrc.zsh" "$HOME/.localrc"
   echo "[ -f \$HOME/.localrc ] && . \$HOME/.localrc" >> "$HOME/.zshrc"
   zsh "$HOME/.zshrc"
 fi
@@ -101,13 +130,15 @@ echo '
 ---------------------------------------------
 '
 
-if !(which docker >/dev/null 2>&1); then
-  curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
-fi
+if [ $INSTALL_DOCKER -eq 1 ]; then
+  if !(which docker >/dev/null 2>&1); then
+    curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
+  fi
 
-if !(which docker-compose >/dev/null 2>&1); then
-  sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
+  if !(which docker-compose >/dev/null 2>&1); then
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+  fi
 fi
 
 echo '
